543986b8db6d8aabd4e1a537a36917055a254004
Gereon V <gereonvincenti@gmx.de>
2020-10-15 18:48:18 +0200

    added method returning random number between to param values (inclusive)

--- app/src/main/java/com/egk/EGK_App/MainActivity.java
+++ app/src/main/java/com/egk/EGK_App/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
@@ -18,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         textView = findViewById(R.id.textView);
+    }
+
+    /**
+     * returns random number from span
+     * @param min kleinste Zahl (inklusive)
+     * @param max größte Zahl (inklusive)
+     */
+    private int rngFromSpan(int min, int max) throws Exception {
+        if(max <= min) throw new Exception("max-min Verhältnis");
+        Random random = new Random();
+        return min + random.nextInt(max - min +1);
     }
 }
