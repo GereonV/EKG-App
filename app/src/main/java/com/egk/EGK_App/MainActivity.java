3f7d4c63be8a53014c9f1db96c981654108749b4
Gereon V <gereonvincenti@gmx.de>
2020-10-15 10:09:22 +0200

    added initViews-Methode

--- app/src/main/java/com/egk/EGK_App/MainActivity.java
+++ app/src/main/java/com/egk/EGK_App/MainActivity.java
@@ -3,12 +3,21 @@ package com.egk.EGK_App;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        textView = findViewById(R.id.textView);
     }
 }
