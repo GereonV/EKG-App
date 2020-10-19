20e75cfdb1c238f4972038d5e4f880caaa9d2ada
Gereon V <gereonvincenti@gmx.de>
2020-10-19 11:05:10 +0200

    vleaned up code

--- app/src/main/java/com/egk/EGK_App/MainActivity.java
+++ app/src/main/java/com/egk/EGK_App/MainActivity.java
@@ -37,23 +37,28 @@ public class MainActivity extends AppCompatActivity {
         try {
             String min = editTextMin.getText().toString();
             String max = editTextMax.getText().toString();
-            if(!max.isEmpty()) {
-                int randomNumber = rngFromSpan(min.isEmpty() ? 1 : Integer.parseInt(min), Integer.parseInt(max));
-                textView.setText(String.valueOf(randomNumber));
-            } else Toast.makeText(this, getString(R.string.emptyInputMessage), Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+            if(!max.isEmpty()) textView.setText(String.valueOf(rngFromSpan(min.isEmpty() ? 1 : Integer.parseInt(min), Integer.parseInt(max))));
+            else toast(getString(R.string.emptyInputMessage), Toast.LENGTH_SHORT);
+        } catch (Exception e) { toast(e.getMessage(), Toast.LENGTH_SHORT); }
     }
 
     /**
      * returns random number from span
-     * @param min kleinste Zahl (inklusive)
-     * @param max größte Zahl (inklusive)
+     * @param min smallest possible outcome (inclusive)
+     * @param max biggest possible outcome (inclusive)
      */
     private int rngFromSpan(int min, int max) throws Exception {
         if(max <= min) throw new Exception(getString(R.string.maxmin));
         Random random = new Random();
         return min + random.nextInt(max - min +1);
+    }
+
+    /**
+     * shows a Toast-message
+     * @param message the message of the Toast
+     * @param length the length the Toast should be shown (Toast constants)
+     */
+    private void toast(String message, int length) {
+        Toast.makeText(this, message, length).show();
     }
 }
